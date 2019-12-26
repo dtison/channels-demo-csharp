72784092f03c8b0e3ecab9000a53f6524e08fc77
David Ison <github@dtison.net>
2019-12-26 13:59:52 -0500

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Channels {
+    class Program {
+        static async Task  Main(string[] args) {
+            const int max = 1000;
+            var channel = Channel.CreateBounded<int>(10);
+            var count = 0;
+
+            _ = Task.Run(async() => {
+                await foreach (var i in channel.Reader.ReadAllAsync())  {
+                    count++;
+                }
+
+            });
+
+
+            var writer = Task.Run(async () => {
+                for (var i = 0; i < max; i++) {
+                    await channel.Writer.WriteAsync(i);
+                }
+                channel.Writer.Complete();
+            });
+
+            await writer;
+            await channel.Reader.Completion;
+      
+           Console.WriteLine("Hello World! Total is {0}", count);
+
+
+        }
+    }
+}
